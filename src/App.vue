0e75f606860f77a464175d82a32b61f5363a7858
Yakson Chan <yaksonchan@blumar.com.br>
2020-07-31 23:17:48 -0300

    Commit inicial do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Menu />
     <router-view/>
   </div>
 </template>
-
+<script>
+import Menu from './components/Menu'
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="menu">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style scoped>
+
+</style>
