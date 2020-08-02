757e8758c68a3932ca56bc5a8b27076d71768b23
Yakson Chan <yaksonchan@blumar.com.br>
2020-08-02 01:43:33 -0300

    Retirando Menu do topo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <Navbar />
     <router-view/>
     <Footer />
   </div>
 </template>
 <script>
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 export default {
   components: {
-    Footer,
-    Navbar
+    Footer
   }
 }
 </script>
