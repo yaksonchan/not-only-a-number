e622b56923968aca9aa54157e457d40869b457a8
Yakson Chan <yaksonchan@blumar.com.br>
2020-08-02 18:08:54 -0300

    Inclusão de ícones e nomes de vítimas  passando na tela

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="statistics">
+      <div class="marqueeBg">
+          <div style="position: absolute;">
+            <marquee scrollamount="6">
+                <div style="margin-left:-500px">
+                {{names.sort(() => Math.random() - 0.5).join('    ')}}
+                </div>
+            </marquee>
+          </div>
+      </div>
     <div class="container">
             <b-row>
                 <b-col lg="12" class="text-center">
-                    <h2 class="section-heading text-light">Estatísticas <i class="material-icons">equalizer</i></h2>
-                    <p class="text-light">Atualizadas em {{ result.updated_at | formatDate }}</p>
+                    <h2 class="section-heading text-light">Estatísticas <i class="material-icons stats">equalizer</i></h2>
+                    <p class="text-light">*Atualizadas em {{ result.updated_at | formatDate }}</p>
                     <hr class="my-4">
                 </b-col>
             </b-row>
@@ -13,30 +22,47 @@
             <b-row>
                 <b-col md="6" lg="3">
                     <div class="mx-auto service-box mt-5">
-                        <h3 class="mb-3 rise-a-bit">Confirmados</h3>
-                        <h4>{{result.confirmed}} seres humanos</h4>
+                        <i data-aos="fade-down" data-aos-delay="50" class="material-icons icon confirmed" style="font-size:70px;">check_circle</i>
+                        <h3 class="mb-3">Confirmados</h3>
+                        <h4>{{result.confirmed}}</h4>
+                        <h4> seres humanos</h4>
                     </div>
                 </b-col>
                 <b-col md="6" lg="3">
                     <div class="mx-auto service-box mt-5">
+                        <i data-aos="fade-down" data-aos-delay="100" data-aos-anchor=".confirmed" class="material-icons icon recovered" style="font-size:70px;">healing</i>
                         <h3 class="mb-3">Recuperados</h3>
-                        <h4>{{result.recovered}} pessoas</h4>
+                        <h4>{{result.recovered}}</h4>
+                        <h4> pessoas</h4>
                     </div>
                 </b-col>
                 <b-col md="6" lg="3">
                     <div class="mx-auto service-box mt-5">
+                        <i data-aos="fade-down" data-aos-delay="150" data-aos-anchor=".recovered" class="material-icons icon actives" style="font-size:70px;">sick</i>
                         <h3 class="mb-3">Ativos</h3>
-                        <h4>{{result.cases}} vidas</h4>
+                        <h4>{{result.cases}}</h4>
+                        <h4> vidas</h4>
                     </div>
                 </b-col>
                 <b-col md="6" lg="3">
                     <div class="mx-auto service-box mt-5">
+                        <i data-aos="fade-down" data-aos-delay="200" data-aos-anchor=".actives" class="material-icons icon deaths" style="font-size:70px;">sentiment_very_dissatisfied</i>
                         <h3 class="mb-3">Mortos</h3>
-                        <h4>{{result.deaths}} amores</h4>
+                        <h4>{{result.deaths}}</h4>
+                        <h4> amores</h4>
                     </div>
                 </b-col>
             </b-row>
         </div>
+        <div class="marqueeBg">
+          <div style="position: absolute; margin-top:50px">
+            <marquee scrollamount="6">
+                <div style="margin-left:-500px">
+                {{names.sort(() => Math.random() - 0.5).join('    ')}}
+                </div>
+            </marquee>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -46,7 +72,16 @@ export default {
   name: 'Statistics',
   data(){
       return {
-          result: null
+          result: null,
+          names: [
+              "João Carlos Fachinello", "Rosa Campagnolo Vaccaro", "Marcelo Rubens Gonçalves", 
+              "Sebastião Bueno da Silva", "Maurício Borges Marques", "Gerônimo Batista Pires", 
+              "Maria Marilene Rufino Lima", "Patrícia Beatriz Albuquerque", "Raul Lima Bastos", 
+              "Leandro José da Silva", "Paulo Márcio Leite", "Flávio Fialho", "Orlando Tavares Pinheiro", 
+              "Francisco Lau", "Edson da Silva Cavalcante", "Danilo Moura", "Danilo Moura", "Leni Peroto Pereira", 
+              "Angela von Nowakonski", "Elenilson Rodrigues de Souza", "Sandra Maria da Silva", "Elisa Inês da Silva",
+               "Manoel Batista Chaves Filho", "Daniele Cristina da Silva Flor", "Ivanir Clementino de Brito"
+          ]
       }
   },
   created(){
@@ -67,11 +102,21 @@ export default {
         font-family: 'Josefin Sans', sans-serif;
     }
 
-    .rise-a-bit {
-        margin-top: -15px;
+    .marqueeBg {
+        position:relative;
+        top:-50px;
+        pointer-events: none;
+        color: #5ce0a0;
+        font-size: 45px;
+        font-weight: bold;
+        
     }
 
-    .material-icons {
+    .icon:hover {
+        margin-top: -10px;
+    }
+
+    .stats {
         color: #4C4C7C;
         font-size: 25px;
     }
@@ -80,5 +125,6 @@ export default {
         font-family: 'Josefin Sans', sans-serif;
         color: rgb(76, 76, 124);
         text-align: center;
+        margin-top: -40px;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import BootstrapVue from 'bootstrap-vue';
 import 'es6-promise/auto';
 import './firebase';
 import { rtdbPlugin } from 'vuefire';
 import moment from 'moment';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 Vue.use(rtdbPlugin);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
+  created () {
+    AOS.init()
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
